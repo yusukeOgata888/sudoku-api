fec0407a63f3c3a13eed364e04d8e35f2f70efeb
緒方雄介 <ogatayuusuke@ogatayuukenoMBP.lan>
2025-04-29 16:52:11 +0900

    add problems

--- db/migrations/000001_create_answer_table.up.sql
+++ db/migrations/000001_create_answer_table.up.sql
@@ -1,5 +1,13 @@
 CREATE TABLE `answers` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `cell_index` int NOT NULL,
   `cell_number` int NOT NULL,
-  `room` int NOT NULL
-)
+  `session_id` varchar(255) NOT NULL
+);
+
+CREATE TABLE `problems` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `cell_index` int NOT NULL,
+  `cell_number` int NOT NULL,
+  `session_id` varchar(255) NOT NULL
+);
